64e45de3ec11803c0d8c1cbd6f70513717e5acc4
aekendig <aekendig@gmail.com>
2021-02-14 09:42:53 -0600

    analyze hydrilla management and cover

--- code/hydrilla_herbicides_analysis.R
+++ code/hydrilla_herbicides_analysis.R
@@ -43,6 +43,13 @@ plant_lw_hyd <- plant_lw %>%
   filter(GenusSpecies == "Hydrilla verticillata") %>%
   mutate(Area_ha = ShapeArea * 100)
 
+# initial visualization
+plant_fwc_hyd %>%
+  filter(SpeciesAcres < 10) %>%
+  ggplot(aes(x = SurveyDate, y = SpeciesAcres, color = as.factor(AreaOfInterestID))) +
+  geom_line(alpha = 0.5) +
+  theme(legend.position = "none")
+
 # see if species frequency depends on area source in FWC
 ggplot(plant_fwc_hyd, aes(x = SpeciesFrequency, y = SpeciesFrequency_ha)) +
   geom_abline(slope = 1, intercept = 0) +
@@ -123,10 +130,10 @@ survey_change_fwc_hyd <- plant_fwc_hyd %>%
   select(AreaOfInterest, AreaOfInterestID, PermanentID, County_FWC, Area_ha, SurveyDate, AreaCovered_ha) %>%
   arrange(AreaOfInterestID, SurveyDate) %>%
   group_by(AreaOfInterestID, PermanentID) %>%
-  mutate(DaysChange = lead(SurveyDate) - SurveyDate,
-         AreaChange_ha = (lead(AreaCovered_ha) - AreaCovered_ha)/as.numeric(DaysChange) * 365,
+  mutate(DaysChange = SurveyDate - lag(SurveyDate),
+         AreaChange_ha = (AreaCovered_ha - lag(AreaCovered_ha))/as.numeric(DaysChange) * 365,
          SpeciesFrequency_ha = AreaCovered_ha/Area_ha,
-         FrequencyChange = (lead(SpeciesFrequency_ha) - SpeciesFrequency_ha)/as.numeric(DaysChange) * 365) %>%
+         FreqChange = (SpeciesFrequency_ha - lag(SpeciesFrequency_ha))/as.numeric(DaysChange) * 365) %>%
   ungroup()
 
 # check ctrl dataset for unique ID-name combos
@@ -152,8 +159,11 @@ non_herb <- c("Mechanical Harvester",
               "Prescribed Fire")
 
 # FWC average herbicide and plants
+# assign 12/31 to the year herbicide was applied
+# the earliest last survey is in April
+# only include herbicide treatments before the last survey (4 month+ lag)
 plant_herb_fwc_hyd <- plant_change_fwc_hyd %>%
-  inner_join(ctrl_old_hyd %>%
+  left_join(ctrl_old_hyd %>%
                filter(AreaTreated_ha > 0) %>%
                group_by(AreaOfInterestID, PermanentID, Year, Area_ha) %>%
                summarise(AreaTreated_ha = sum(AreaTreated_ha)) %>%
@@ -168,16 +178,101 @@ plant_herb_fwc_hyd <- plant_change_fwc_hyd %>%
                       YearTreated = paste0(Year, "-12-31") %>% as.Date("%Y-%m-%d"),
                       MinYear = min(Year)) %>%
                select(-c(Area_ha, Year))) %>%
-  filter(YearTreated < LastSurvey) %>%
-  mutate(YearsTotal = year(LastSurvey) - MinYear - 1) %>%
+  mutate(AreaTreated_ha = case_when(YearTreated >= LastSurvey ~ 0,
+                                    is.na(YearTreated) ~ 0,
+                                    TRUE ~ AreaTreated_ha),
+         YearTreated = case_when(AreaTreated_ha == 0 ~ NA_Date_,
+                                 TRUE ~ YearTreated),
+         MinYear = case_when(AreaTreated_ha == 0 ~ NA_real_,
+                             TRUE ~ MinYear),
+         YearsTotal = case_when(AreaTreated_ha > 0 ~ year(LastSurvey) - MinYear,
+                                TRUE ~ 1)) %>%
+  unique() %>%
+  group_by(AreaOfInterest, AreaOfInterestID, PermanentID) %>%
+  mutate(rows = n()) %>%
+  ungroup() %>%
+  filter(!(AreaTreated_ha == 0 & rows > 1)) %>%
   group_by(AreaOfInterest, AreaOfInterestID, PermanentID, County_FWC, Area_ha, AreaInitial_ha, AreaChange_ha, FreqChange, DaysChange) %>%
-  summarise(TreatmentYears = unique(YearsTotal),
-            TreatmentFrequency = n()/unique(YearsTotal),
+  summarise(TreatmentYears = case_when(sum(AreaTreated_ha) > 0 ~ unique(YearsTotal),
+                                       TRUE ~ 0),
+            TreatmentFrequency = sum(AreaTreated_ha > 0)/unique(YearsTotal),
             TreatmentIntensity = mean(AreaTreated_ha/Area_ha)) %>%
   ungroup()
 
+# FWC survey-to-survey herbicide and plants
+survey_herb_fwc_hyd <- survey_change_fwc_hyd %>%
+  left_join(ctrl_old_hyd %>%
+              filter(AreaTreated_ha > 0) %>%
+              group_by(AreaOfInterestID, PermanentID, Year, Area_ha) %>%
+              summarise(AreaTreated_ha = sum(AreaTreated_ha)) %>%
+              ungroup() %>%
+              full_join(ctrl_new_hyd %>%
+                          filter(AreaTreated_ha > 0 & !is.na(ControlMethod) & !(ControlMethod %in% non_herb)) %>%
+                          group_by(AreaOfInterestID, PermanentID, Year, Area_ha) %>%
+                          summarise(AreaTreated_ha = sum(AreaTreated_ha)) %>%
+                          ungroup()) %>%
+              mutate(AreaTreated_ha = case_when(AreaTreated_ha > Area_ha ~ Area_ha,
+                                                TRUE ~ AreaTreated_ha),
+                     YearTreated = paste0(Year, "-12-31") %>% as.Date("%Y-%m-%d")) %>%
+              select(-c(Area_ha, Year))) %>%
+  mutate(SurvTreatDays = SurveyDate - YearTreated,
+         AreaTreated_ha = case_when(SurvTreatDays > 0 & SurvTreatDays <= DaysChange ~ AreaTreated_ha,
+                                    TRUE ~ 0),
+         SurvTreatDays = case_when(AreaTreated_ha == 0 ~ NA_real_,
+                                   TRUE ~ as.numeric(SurvTreatDays)),
+         YearTreated = case_when(AreaTreated_ha == 0 ~ NA_Date_,
+                                 TRUE ~ YearTreated)) %>%
+  unique() %>%
+  group_by(AreaOfInterest, AreaOfInterestID, PermanentID, SurveyDate) %>%
+  mutate(rows = n()) %>%
+  ungroup() %>%
+  filter(!(AreaTreated_ha == 0 & rows > 1)) %>%
+  group_by(AreaOfInterest, AreaOfInterestID, PermanentID, County_FWC, Area_ha, SurveyDate, AreaCovered_ha, DaysChange, AreaChange_ha, SpeciesFrequency_ha, FreqChange) %>%
+  summarise(TreatmentYears = case_when(sum(AreaTreated_ha) > 0 ~ unique(as.numeric(DaysChange)/365),
+                                       TRUE ~ 0),
+            TreatmentFrequency = sum(AreaTreated_ha > 0)/unique(as.numeric(DaysChange) * 365),
+            TreatmentIntensity = mean(AreaTreated_ha/Area_ha)) %>%
+  ungroup()
 
-#### start here: add treatment data to survey-to-survey data ####
+# FWC survey-to-survey cumulative herbicide and plants
+cltv_herb_fwc_hyd <- survey_change_fwc_hyd %>%
+  left_join(ctrl_old_hyd %>%
+              filter(AreaTreated_ha > 0) %>%
+              group_by(AreaOfInterestID, PermanentID, Year, Area_ha) %>%
+              summarise(AreaTreated_ha = sum(AreaTreated_ha)) %>%
+              ungroup() %>%
+              full_join(ctrl_new_hyd %>%
+                          filter(AreaTreated_ha > 0 & !is.na(ControlMethod) & !(ControlMethod %in% non_herb)) %>%
+                          group_by(AreaOfInterestID, PermanentID, Year, Area_ha) %>%
+                          summarise(AreaTreated_ha = sum(AreaTreated_ha)) %>%
+                          ungroup()) %>%
+              mutate(AreaTreated_ha = case_when(AreaTreated_ha > Area_ha ~ Area_ha,
+                                                TRUE ~ AreaTreated_ha),
+                     YearTreated = paste0(Year, "-12-31") %>% as.Date("%Y-%m-%d"),
+                     MinYear = min(Year)) %>%
+              select(-c(Area_ha, Year))) %>%
+  mutate(SurvTreatDays = SurveyDate - YearTreated,
+         AreaTreated_ha = case_when(SurvTreatDays > 0 & !is.na(DaysChange) ~ AreaTreated_ha,
+                                    TRUE ~ 0),
+         SurvTreatDays = case_when(AreaTreated_ha == 0 ~ NA_real_,
+                                   TRUE ~ as.numeric(SurvTreatDays)),
+         YearTreated = case_when(AreaTreated_ha == 0 ~ NA_Date_,
+                                 TRUE ~ YearTreated),
+         MinYear = case_when(AreaTreated_ha == 0 ~ NA_real_,
+                             TRUE ~ MinYear)) %>%
+  unique() %>%
+  group_by(AreaOfInterest, AreaOfInterestID, PermanentID, SurveyDate) %>%
+  mutate(rows = n()) %>%
+  ungroup() %>%
+  filter(!(AreaTreated_ha == 0 & rows > 1)) %>%
+  group_by(AreaOfInterest, AreaOfInterestID, PermanentID, County_FWC, Area_ha, SurveyDate, AreaCovered_ha, DaysChange, AreaChange_ha, SpeciesFrequency_ha, FreqChange) %>%
+  summarise(TreatmentYears = case_when(sum(AreaTreated_ha) > 0 ~ unique(year(SurveyDate) - MinYear),
+                                       TRUE ~ 0),
+            TreatmentFrequency = case_when(sum(AreaTreated_ha) > 0 ~ sum(AreaTreated_ha > 0)/TreatmentYears,
+                                           TRUE ~ 0),
+            TreatmentIntensity = mean(AreaTreated_ha/Area_ha)) %>%
+  ungroup()
+  
 
 
 #### plant_change_fwc_hyd exploratory figures ####
@@ -200,18 +295,18 @@ ggplot(plant_change_fwc_hyd, aes(DaysChange, FreqChange_ha)) +
 ggplot(survey_change_fwc_hyd, aes(x = AreaChange_ha)) +
   geom_histogram()
 
-ggplot(survey_change_fwc_hyd, aes(x = FrequencyChange)) +
+ggplot(survey_change_fwc_hyd, aes(x = FreqChange)) +
   geom_histogram()
 
-ggplot(survey_change_fwc_hyd, aes(Area_ha, FrequencyChange)) +
+ggplot(survey_change_fwc_hyd, aes(Area_ha, FreqChange)) +
   geom_point() +
   geom_smooth(method = "lm")
 
-ggplot(survey_change_fwc_hyd, aes(log(Area_ha), FrequencyChange)) +
+ggplot(survey_change_fwc_hyd, aes(log(Area_ha), FreqChange)) +
   geom_point() +
   geom_smooth(method = "lm")
 
-ggplot(survey_change_fwc_hyd, aes(SpeciesFrequency_ha, FrequencyChange)) +
+ggplot(survey_change_fwc_hyd, aes(SpeciesFrequency_ha, FreqChange)) +
   geom_point() +
   geom_smooth(method = "lm")
 
@@ -256,3 +351,65 @@ ggplot(plant_herb_fwc_hyd, aes(TreatmentFrequency, FreqChange)) +
 
 ggplot(plant_herb_fwc_hyd, aes(TreatmentIntensity, FreqChange)) +
   geom_point()
+
+
+#### survey_herb_fwc_hyd exploratory figures ####
+
+ggplot(survey_herb_fwc_hyd, aes(TreatmentFrequency, AreaChange_ha)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = F)
+
+ggplot(survey_herb_fwc_hyd, aes(TreatmentIntensity, AreaChange_ha)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = F)
+
+ggplot(survey_herb_fwc_hyd, aes(TreatmentFrequency, FreqChange)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = F)
+
+survey_herb_fwc_hyd %>%
+  filter(TreatmentFrequency > 0) %>%
+  ggplot(aes(TreatmentFrequency, FreqChange)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = F)
+
+ggplot(survey_herb_fwc_hyd, aes(TreatmentIntensity, FreqChange)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = F)
+
+survey_herb_fwc_hyd %>%
+  filter(TreatmentIntensity > 0) %>%
+  ggplot(aes(TreatmentIntensity, FreqChange)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = F)
+
+
+#### cltv_herb_fwc_hyd exploratory figures ####
+
+ggplot(cltv_herb_fwc_hyd, aes(TreatmentFrequency, AreaChange_ha)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = F)
+
+ggplot(cltv_herb_fwc_hyd, aes(TreatmentIntensity, AreaChange_ha)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = F)
+
+ggplot(cltv_herb_fwc_hyd, aes(TreatmentFrequency, FreqChange)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = F)
+
+cltv_herb_fwc_hyd %>%
+  filter(TreatmentFrequency > 0) %>%
+  ggplot(aes(TreatmentFrequency, FreqChange)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = F)
+
+ggplot(cltv_herb_fwc_hyd, aes(TreatmentIntensity, FreqChange)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = F)
+
+cltv_herb_fwc_hyd %>%
+  filter(TreatmentIntensity > 0) %>%
+  ggplot(aes(TreatmentIntensity, FreqChange)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = F)
