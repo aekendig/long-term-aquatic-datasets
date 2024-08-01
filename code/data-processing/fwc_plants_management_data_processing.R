e57ebbcdd58e31bc07a64e908932b6f43a722920
Amy Kendig <aekendig@gmail.com>
2024-08-01 07:02:50 -0500

    format plant data for analyses

--- code/data-processing/fwc_plants_management_data_processing.R
+++ code/data-processing/fwc_plants_management_data_processing.R
@@ -55,7 +55,7 @@ plants2 <- plants %>%
 
 # waterbodies that were surveyed
 waterbodies <- plants2 %>%
-  distinct(PermanentID, AreaOfInterest, AreaOfInterestID)
+  distinct(PermanentID, AreaOfInterest, AreaOfInterestID, County)
 
 # can I use AOI ID?
 n_distinct(waterbodies$AreaOfInterestID)
@@ -64,15 +64,30 @@ n_distinct(waterbodies$AreaOfInterestID) == nrow(waterbodies)
 
 # plant surveys
 plant_surv <- plants2 %>%
-  distinct(PermanentID, AreaOfInterest, AreaOfInterestID, SurveyYear, SurveyDate)
+  distinct(PermanentID, AreaOfInterest, AreaOfInterestID, County, WaterbodyAcres, 
+           SurveyYear, SurveyDate)
 
 # select waterbodies from management data with surveys
 mgmt2 <- mgmt %>%
-  inner_join(waterbodies) %>%
+  inner_join(waterbodies)
+
+# duplicate rows with floating plants
+# add treatment targets
+mgmt3 <- mgmt2 %>%
+  mutate(Species = if_else(Species == "Floating Plants (Eichhornia and Pistia)", 
+                           "Floating (Eichhornia)",
+                           Species)) %>%
+  full_join(mgmt2 %>%
+              mutate(Species = if_else(Species == "Floating Plants (Eichhornia and Pistia)", 
+                                       "Floating (Pistia)",
+                                       Species))) %>%
   mutate(TreatmentTarget = case_when(Species == "Hydrilla verticillata" ~ "hydrilla", 
-                                     Species %in% c("Eichhornia crassipes", "Floating Plants (Eichhornia and Pistia)") ~ "hyacinth", 
-                                     Species %in% c("Pistia stratiotes", "Floating Plants (Eichhornia and Pistia)") ~ "lettuce",
+                                     Species %in% c("Eichhornia crassipes", "Floating (Eichhornia)") ~ "hyacinth", 
+                                     Species %in% c("Pistia stratiotes", "Floating (Pistia)") ~ "lettuce",
                                      TRUE ~ "other"))
+
+# check that the row duplication worked correctly
+filter(mgmt2, Species == "Floating Plants (Eichhornia and Pistia)") %>% nrow() == nrow(mgmt3) - nrow(mgmt2)
 
 # were any waterbodies not treated?
 (plant_no_mgmt <- plant_surv %>%
@@ -128,13 +143,13 @@ waterbodies2 <- waterbodies %>%
 #### summarize all management data ####
 
 # whether or not management occurred each year
-mgmt_year <- mgmt2 %>%
+mgmt_year <- mgmt3 %>%
   left_join(waterbodies2) %>%
   filter(TreatmentDate <= MaxSurveyDate) %>% # remove dates older than the max survey date for each waterbody (old management, date = 1/1)
   distinct(PermanentID, AreaOfInterest, AreaOfInterestID, TreatmentYear, TreatmentTarget) %>% # isolate waterbodies and years in management dataset
   mutate(Treatment = 1) %>%
   full_join(plant_surv %>%
-              distinct(PermanentID, AreaOfInterest, AreaOfInterestID) %>% # add all waterbodies from the plant survey dataset
+              distinct(PermanentID, AreaOfInterest, AreaOfInterestID, County) %>% # add all waterbodies from the plant survey dataset
               expand_grid(TreatmentYear = min(mgmt2$TreatmentYear):max(mgmt2$TreatmentYear)) %>% # expanded by all years in the management data
               expand_grid(TreatmentTarget = c("hydrilla", "hyacinth", "lettuce", "other"))) %>% # and all targets
   mutate(Treatment = replace_na(Treatment, 0)) %>%
@@ -150,12 +165,113 @@ anti_join(waterbodies2, mgmt_year)
 waterbodies3 <- waterbodies2 %>%
   filter(AreaOfInterestID %in% mgmt_year$AreaOfInterestID)
 
+# duplicate lake names? need to specify county
+wb3_dup_lakes <- waterbodies3 %>%
+  distinct(PermanentID, AreaOfInterest, AreaOfInterestID, County) %>%
+  get_dupes(AreaOfInterest)
+
 # for each waterbody, number of management years by target
 mgmt_year_sum <- mgmt_year %>%
-  group_by(PermanentID, AreaOfInterest, AreaOfInterestID, TreatmentTarget) %>%
+  group_by(PermanentID, AreaOfInterest, AreaOfInterestID, County, TreatmentTarget) %>%
   summarize(TreatmentYears = sum(Treatment),
-            TotalYears = n_distinct(TreatmentYear),
+            TotalTreatmentYears = n_distinct(TreatmentYear),
+            .groups = "drop") %>%
+  full_join(mgmt_year %>%
+              filter(TreatmentTarget %in% c("hyacinth", "lettuce")) %>%
+              group_by(PermanentID, AreaOfInterest, AreaOfInterestID, County, TreatmentYear) %>% # summarize by waterbody and year
+              summarize(Treatment = if_else(sum(Treatment) > 0, 1, 0), # if either species was treated, count
+                        .groups = "drop") %>%
+              group_by(PermanentID, AreaOfInterest, AreaOfInterestID, County) %>% # summarize by waterbody
+              summarize(TreatmentYears = sum(Treatment),
+                        TotalTreatmentYears = n_distinct(TreatmentYear),
+                        .groups = "drop") %>%
+              mutate(TreatmentTarget = "floating")) %>%
+  mutate(WaterbodyName = if_else(AreaOfInterest %in% wb3_dup_lakes$AreaOfInterest,
+                                 paste0(AreaOfInterest, " (", str_to_sentence(County), ")"),
+                                 AreaOfInterest),
+         PropTreatmentYears = TreatmentYears / TotalTreatmentYears)
+
+# waterbodies
+n_distinct(mgmt_year_sum$AreaOfInterestID) # 386
+# targets
+n_distinct(mgmt_year_sum$TreatmentTarget) # 5
+# check rows
+nrow(mgmt_year_sum) == n_distinct(mgmt_year_sum$AreaOfInterestID) * n_distinct(mgmt_year_sum$TreatmentTarget)
+
+# distribution of values
+ggplot(mgmt_year_sum, aes(x = PropTreatmentYears)) +
+  geom_histogram(binwidth = 0.1)
+
+
+#### summarize all invasive plant data ####
+
+# select lakes and years in management data time span
+plants3 <- plants2 %>%
+  filter(AreaOfInterestID %in% waterbodies3$AreaOfInterestID &
+           SurveyYear >= min(mgmt_year$TreatmentYear) &
+           SurveyYear <= max(mgmt_year$TreatmentYear))
+
+# select target species
+# calculate proportion waterbody covered
+inv_plants <- plants3 %>%
+  filter(TaxonName %in% c("Hydrilla verticillata", "Eichhornia crassipes", "Pistia stratiotes")) %>%
+  mutate(PropSpeciesAcres = SpeciesAcres / WaterbodyAcres,
+         TreatmentTarget = case_when(TaxonName == "Hydrilla verticillata" ~ "hydrilla", 
+                                     TaxonName == "Eichhornia crassipes" ~ "hyacinth", 
+                                     TaxonName == "Pistia stratiotes" ~ "lettuce"))
+
+# waterbodies
+n_distinct(inv_plants$AreaOfInterestID) # 386
+# species
+n_distinct(inv_plants$TaxonName) # 3
+
+# combine with management
+inv_plants_mgmt <- left_join(inv_plants, mgmt_year_sum) %>%
+  mutate(PropTreatmentYearsF = as.factor(round_half_up(PropTreatmentYears, 1)))
+
+# visualize
+ggplot(inv_plants_mgmt, aes(x = SurveyYear, y = PropSpeciesAcres, group = AreaOfInterestID)) +
+  geom_smooth(method = "lm", se = F, linewidth = 0.5) +
+  facet_grid(TreatmentTarget ~ PropTreatmentYearsF, scales = "free")
+
+# save
+write_csv(inv_plants_mgmt, "intermediate-data/FWC_invasive_plant_management_long_term.csv")
+
+
+#### summarize all native plant data ####
+
+# select native plants
+native_plants <- plants3 %>%
+  filter(Origin == "Native" & str_detect(TaxonName, "spp.") == F &
+           TaxonName != "Filamentous algae")
+
+# see if any are very rare
+native_plants_freq <- native_plants %>%
+  filter(IsDetected == "Yes") %>%
+  group_by(TaxonName) %>%
+  summarize(Waterbodies = n_distinct(AreaOfInterestID),
+            Years = n_distinct(SurveyYear),
+            Occurrences = n_distinct(paste(AreaOfInterestID, SurveyYear, sep = "_")),
             .groups = "drop")
+
+ggplot(native_plants_freq, aes(x = Waterbodies)) +
+  geom_histogram(binwidth = 1)
+min(native_plants_freq$Waterbodies)
+
+ggplot(native_plants_freq, aes(x = Years)) +
+  geom_histogram(binwidth = 1)
+min(native_plants_freq$Years)
+
+ggplot(native_plants_freq, aes(x = Occurrences)) +
+  geom_histogram(binwidth = 10)
+min(native_plants_freq$Occurrences)
+
+ggplot(native_plants_freq, aes(x = Waterbodies, y = Occurrences)) +
+  geom_point() +
+  geom_abline(intercept = 0, slope = 5) # number of years present
+# there are a handful with waterbodies < 50
+# look at these more closely
+
 
 
 #### summarize new management data by survey-span ####
@@ -197,14 +313,17 @@ plant_surv2_new <- plant_surv2 %>%
 mgmt_between <- left_join(mgmt2_new, plant_surv2_new, relationship = "many-to-many") %>%
   filter(TreatmentDate >= LastSurveyDate & TreatmentDate <= SurveyDate) %>% # select management between surveys
   full_join(plant_surv2_new %>% # add all surveys (management will be NA)
-              expand_grid(TreatmentTarget = c("hydrilla", "hyacinth", "lettuce", "other"))) # expanded by all targets
+              expand_grid(TreatmentTarget = c("hydrilla", "hyacinth", "lettuce", "other"))) %>% # expanded by all targets
+  mutate(WaterbodyName = if_else(AreaOfInterest %in% wb3_dup_lakes$AreaOfInterest,
+                                 paste0(AreaOfInterest, " (", str_to_sentence(County), ")"),
+                                 AreaOfInterest))
 
 # for each survey, what management methods were used?
 mgmt_between_method <- mgmt_between %>%
   mutate(MethodHerbicide = replace_na(MethodHerbicide, "none") %>%
-           fct_recode(yes = "unknown")) %>%
-  count(PermanentID, AreaOfInterest, AreaOfInterestID, SurveyDate, LastSurveyDate, BetweenSurveyDays, 
-           TreatmentTarget, MethodHerbicide) %>% # 7 cases, seems more likely they'd be herbicide
+           fct_recode(yes = "unknown")) %>% # 7 cases, seems more likely they'd be herbicide
+  count(PermanentID, AreaOfInterest, AreaOfInterestID, County, WaterbodyName, SurveyDate, LastSurveyDate, 
+        BetweenSurveyDays, TreatmentTarget, MethodHerbicide) %>%
   pivot_wider(names_from = MethodHerbicide,
               values_from = n) %>%
   mutate(across(.cols = c(none, yes, no), .fns = ~replace_na(.x, 0)),
@@ -213,94 +332,54 @@ mgmt_between_method <- mgmt_between %>%
          Herbicide = yes,
          NonHerbicide = no)
 
-#### start here: herbicide timing and area ####
-
-
-### older code - potentially useful, check before deleting ####
-
-
-# duplicate lake names? need to specify county
-mgmt_dup_lakes <- mgmt2 %>%
-  distinct(PermanentID, AreaOfInterest, AreaOfInterestID) %>%
-  get_dupes(AreaOfInterest)
-
-# restrict mgmt to before last survey
-# identify treatment targets
-mgmt3 <- mgmt2 %>%
-  left_join(plant_surv_max) %>%
-  filter(TreatmentDate <= MaxSurveyDate) %>%
-  mutate(TreatmentTarget = case_when(Species == "Hydrilla verticillata" ~ "hydrilla", 
-                                     Species == "Eichhornia crassipes" ~ "hyacinth", 
-                                     Species == "Pistia stratiotes" ~ "lettuce",
-                                     Species == "Floating Plants (Eichhornia and Pistia)" ~ "floating",
-                                     TRUE ~ "other"),
-         Waterbody = if_else(AreaOfInterest %in% mgmt_dup_lakes$AreaOfInterest,
-                             paste0(AreaOfInterest, " (", str_to_title(County_FWC), ")"),
-                             AreaOfInterest))
-
-# for the new dataset, how many treatments does each waterbody have until the
-# last survey, and how do these breakdown by target?
-mgmt_new_trts_sum <- mgmt3 %>%
-  filter(CtrlSet == "new") %>%
-  group_by(PermanentID, AreaOfInterest, AreaOfInterestID, Waterbody, TreatmentTarget) %>%
-  summarize(Treatments = n_distinct(TreatmentID),
+# for each survey with herbicide used in between, when was it applied?
+mgmt_between_time <- mgmt_between %>%
+  filter(MethodHerbicide == "yes") %>%
+  count(PermanentID, AreaOfInterest, AreaOfInterestID, County, WaterbodyName, SurveyDate, LastSurveyDate, 
+        BetweenSurveyDays, TreatmentTarget, TreatmentMonth) %>% # number of treatments per month
+  mutate(TreatmentQuarter = case_when(TreatmentMonth %in% 1:3 ~ "Q1",
+                                      TreatmentMonth %in% 4:6 ~ "Q2",
+                                      TreatmentMonth %in% 7:9 ~ "Q3",
+                                      TreatmentMonth %in% 10:12 ~ "Q4")) %>%
+  group_by(PermanentID, AreaOfInterest, AreaOfInterestID, County, SurveyDate, LastSurveyDate, 
+           BetweenSurveyDays, TreatmentTarget, TreatmentQuarter) %>%
+  summarize(n = sum(n), # number of treatments per quarter
             .groups = "drop") %>%
-  full_join(mgmt3 %>% # combine all floating
-              filter(CtrlSet == "new" & TreatmentTarget %in% c("hyacinth", "lettuce", "floating")) %>%
-              mutate(TreatmentTarget = "floating_combined") %>%
-              group_by(PermanentID, AreaOfInterest, AreaOfInterestID, Waterbody, TreatmentTarget) %>%
-              summarize(Treatments = n_distinct(TreatmentID),
-                        .groups = "drop")) %>%
-  full_join(mgmt3 %>% # combine all treatments
-              filter(CtrlSet == "new") %>%
-              mutate(TreatmentTarget = "total") %>%
-              group_by(PermanentID, AreaOfInterest, AreaOfInterestID, Waterbody, TreatmentTarget) %>%
-              summarize(Treatments = n_distinct(TreatmentID),
-                        .groups = "drop")) %>%
-  full_join(mgmt3 %>% # all combinations to add zeros
-              filter(CtrlSet == "new") %>%
-              distinct(PermanentID, AreaOfInterest, AreaOfInterestID, Waterbody) %>%
-              expand_grid(TreatmentTarget = c("hydrilla", "hyacinth", "lettuce", 
-                                              "floating", "other", "floating_combined", "total"))) %>%
-  mutate(Treatments = replace_na(Treatments, 0))
+  pivot_wider(names_from = TreatmentQuarter,
+              values_from = n) %>%
+  mutate(across(.cols = starts_with("Q"), .fns = ~ replace_na(.x, 0)))
 
-# how many treatment years per waterbody by target?
-mgmt_yrs_sum <- mgmt3 %>%
-  group_by(PermanentID, AreaOfInterest, AreaOfInterestID, Waterbody, TreatmentTarget) %>%
-  summarize(TreatmentYears = n_distinct(TreatmentYear),
-            .groups = "drop") %>%
-  full_join(mgmt3 %>% # combine all floating
-              filter(TreatmentTarget %in% c("hyacinth", "lettuce", "floating")) %>%
-              mutate(TreatmentTarget = "floating_combined") %>%
-              group_by(PermanentID, AreaOfInterest, AreaOfInterestID, Waterbody, TreatmentTarget) %>%
-              summarize(TreatmentYears = n_distinct(TreatmentYear),
-                        .groups = "drop")) %>%
-  full_join(mgmt3 %>% # combine all treatments
-              mutate(TreatmentTarget = "total") %>%
-              group_by(PermanentID, AreaOfInterest, AreaOfInterestID, Waterbody, TreatmentTarget) %>%
-              summarize(TreatmentYears = n_distinct(TreatmentYear),
-                        .groups = "drop")) %>%
-  full_join(mgmt3 %>% # all combinations to add zeros
-              distinct(PermanentID, AreaOfInterest, AreaOfInterestID, Waterbody) %>%
-              expand_grid(TreatmentTarget = c("hydrilla", "hyacinth", "lettuce", 
-                                              "floating", "other", "floating_combined", "total"))) %>%
-  mutate(TreatmentYears = replace_na(TreatmentYears, 0))
+ggplot(mgmt_between_time) +
+  geom_density(aes(x = log(Q1 + 1))) +
+  geom_density(aes(x = log(Q2 + 1)), color = "blue") +
+  geom_density(aes(x = log(Q3 + 1)), color = "green") +
+  geom_density(aes(x = log(Q4 + 1)), color = "yellow") +
+  facet_wrap(~ TreatmentTarget)
 
-# visualize
-ggplot(mgmt_new_trts_sum, aes(x = Treatments)) +
+# for each survey with herbicide used in between how much was treated?
+mgmt_between_space <- mgmt_between %>%
+  filter(MethodHerbicide == "yes") %>%
+  mutate(PropTreated = TotalAcres / WaterbodyAcres) %>%
+  group_by(PermanentID, AreaOfInterest, AreaOfInterestID, County, WaterbodyName, SurveyDate, LastSurveyDate, 
+           BetweenSurveyDays, TreatmentTarget) %>%
+  summarize(PropTreated = mean(PropTreated),
+            .groups = "drop")
+
+ggplot(mgmt_between_space, aes(x = log(PropTreated))) +
   geom_density() +
-  facet_wrap(~ TreatmentTarget, scales = "free") +
-  def_theme
+  facet_wrap(~ TreatmentTarget)
 
-ggplot(mgmt_yrs_sum, aes(x = TreatmentYears)) +
-  geom_density() +
-  facet_wrap(~ TreatmentTarget, scales = "free") +
-  def_theme
+# combine space and time data
+mgmt_between_herb <- full_join(mgmt_between_space, mgmt_between_time) %>%
+  mutate(LogPropTreated = log(PropTreated),
+         LogQ1 = log(Q1 + 1),
+         LogQ2 = log(Q2 + 1),
+         LogQ3 = log(Q3 + 1),
+         LogQ4 = log(Q4 + 1))
 
-# look at some of the high values
-(temp_high_floating <- filter(mgmt_new_trts_sum, 
-                              TreatmentTarget == "floating" & Treatments > 2000) %>%
-    inner_join(mgmt3))
-# multiple treatments within the same day using different amounts of total herbicide or total acres treated,
-# probably treating different areas of the lake
-# need a more specific definition of a treatment than a unique treatment ID
+#### start here: summarize plant responses ####
+
+
+
+
+
